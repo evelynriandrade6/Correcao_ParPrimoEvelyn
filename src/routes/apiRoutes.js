f6249ec589d680a1cf7d4a07c5adf605b6fadbf2
evelynriandrade6 <eveyandrade0.3@gmail.com>
2024-09-18 09:39:15 -0300

    18/09

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -2,5 +2,9 @@
 //Router será utilizada para definir rotas específicas
 const router = require('express').Router();
 
+const controllerNumbers = require("../controllers/controllerNumbers")
+
+router.post("/par/", controllerNumbers.checkPar);
+router.post("/primo/", controllerNumbers.checkPrimo);
 
 module.exports = router
